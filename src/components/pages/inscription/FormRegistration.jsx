57d83b6544b3fd30e789820d261bbb884bf1c5e8
Maurikunin <mauri.kunin17@gmail.com>
2024-09-06 01:08:45 +0200

    errores

--- src/components/pages/inscription/FormRegistration.jsx
+++ src/components/pages/inscription/FormRegistration.jsx
@@ -13,7 +13,8 @@ import axios from "axios";
 import { categorias } from "../../../utils/categorias"; 
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import TextInput from "./textInput.jsx";
+import TextInput from "./TextInput.jsx";
+
 
 const FormRegistration = () => {
   const [category, setCategory] = useState([]);

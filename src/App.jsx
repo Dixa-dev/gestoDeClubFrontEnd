4b3b5c0874a2dfdfd090d00db0bbb807ad609f12
Maurikunin <mauri.kunin17@gmail.com>
2024-09-06 00:55:36 +0200

    otra vez

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import FormRegistration from "./components/pages/inscription/FormRegistration.jsx"
-import ListPlayers from "./components/pages/players/listPlayer.jsx"
+import ListPlayers from "./components/pages/players/ListPlayer.jsx"
 import NavBar from "./components/layout/navBar/NavBar.jsx"
 import Home from "./components/pages/home/Home.jsx"
 

622bb5e13704417e7a76f76a4b78e4e8abaea26b
Maurikunin <mauri.kunin17@gmail.com>
2024-09-06 00:49:35 +0200

    ya estoy cansado de comitear

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import FormRegistration from "./components/pages/inscription/FormRegistration"
-import ListPlayers from "./components/pages/players/listPlayers"
-import NavBar from "./components/layout/navBar/NavBar"
-import Home from "./components/pages/home/Home"
+import FormRegistration from "./components/pages/inscription/FormRegistration.jsx"
+import ListPlayers from "./components/pages/players/listPlayer.jsx"
+import NavBar from "./components/layout/navBar/NavBar.jsx"
+import Home from "./components/pages/home/Home.jsx"
 
 function App() {
 

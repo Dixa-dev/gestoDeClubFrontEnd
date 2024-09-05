d333af0dbf1305117a566c788f625428e8cde2fb
Maurikunin <mauri.kunin17@gmail.com>
2024-09-06 01:01:42 +0200

    arreglando players

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import FormRegistration from "./components/pages/inscription/FormRegistration.jsx"
-import ListPlayers from "./components/pages/players/ListPlayer.jsx"
 import NavBar from "./components/layout/navBar/NavBar.jsx"
 import Home from "./components/pages/home/Home.jsx"
+import PlayerList from "./components/pages/players/PlayerList.jsx"
 
 function App() {
 
@@ -13,8 +13,8 @@ function App() {
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/register" element={<FormRegistration />} />
-      <Route path="/players" element={<ListPlayers />}>
-      </Route>
+      <Route path="/players" element={<PlayerList />}/>
+     
 
       </Routes>
       </BrowserRouter>

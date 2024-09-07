bef2f55ef3cd334895ddb90eb144e9e75fbddfda
Maurikunin <mauri.kunin17@gmail.com>
2024-09-07 12:46:19 +0200

    seguimos arreglando

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -52,7 +52,7 @@ const Home = () => {
           >
             <Typography
               variant="h1"
-              sx={{ fontWeight: "950", fontSize: { xs: "1.8em", md: "6rem" } }}
+              sx={{ fontWeight: "950", fontSize: { xs: "1.9em", md: "6rem" } }}
             >
               Bienvenidos a la web de la Coop!!
             </Typography>
